29ce29257325fd0798056cd34231fa3ce34b726c
Kayla Budzeak <kaylahmg@gmail.com>
2020-09-16 21:51:42 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,44 @@
+class Song
+attr_accessor :name, :genre, :artist
+
+@@count = 0
+@@artists = []
+@@genres = []
+
+  def initialize(name, artist, genre)
+    @name = name
+    @artist = artist
+    @genre = genre
+    @@count += 1
+    @@genres << genre
+    @@artists << artist
+  end
+
+  def self.count
+    @@count
+  end
+
+  def self.artists
+    @@artists.uniq
+  end
+
+  def self.genres
+    @@genres.uniq
+  end
+
+  def self.genre_count
+    genre_count =  {}
+    @@genres.each do |genre|
+      if genre_count[genre]
+        genre_count[genre] += 1
+      else
+        genre_count[genre] = 1
+      end
+    genre_count
+    end
+  end
+
+  def self.artist_count
+  end
+
+end
